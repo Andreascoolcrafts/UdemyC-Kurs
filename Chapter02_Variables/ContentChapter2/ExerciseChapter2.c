ff5f889f93c61a787932ff116aec4a0cb294cf87
Andreas Rittsel <andreas@rittsel.de>
2022-10-11 17:06:29 +0200

    finished first exercise (02 Variables)

--- Chapter02_Variables/ContentChapter2/ExerciseChapter2.c
+++ Chapter02_Variables/ContentChapter2/ExerciseChapter2.c
@@ -2,7 +2,17 @@
 
 int main()
 {
-
-
+    int age;
+    printf("Bitte gib dein aktuelles Alter ein: ");
+    scanf("%d", &age);
+    printf("Du bist %d Jahre alt.\n", age);
+    int ageInYears = age * 365;
+    printf("Du lebst seit %d Jahren.\n", ageInYears);
+    int ageInDays = ageInYears * 24;
+    printf("Du lebst seit %d Tagen.\n", ageInDays);
+    int ageInHours = ageInDays * 60;
+    printf("Du lebst seit %d Stunden.\n", ageInHours);
+    int ageInMinuts = ageInHours * 60;
+    printf("Du lebst seit %d Minuten.\n", ageInMinuts);
     return 0;
 }
